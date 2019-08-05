e35642dc88a313979b71ed43fc82e1a5fbda7a04
guoqiang <isguoqiang.liu@gmail.com>
2019-08-05 16:06:10 +0800

    20190805

---
+++ linkedList/110_Balanced_Binary_Tree.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+// 利用 maxDepth 计算的程序
+// 设置一个isBalanced 全局变量
+class Solution {
+    private boolean isBalanced = true;
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) { return true; }
+        int depth = isBalancedHelper(root);
+        return isBalanced;
+    }
+    private int isBalancedHelper(TreeNode root) {
+        if (root == null) { return 0; }
+        int leftDepth = isBalancedHelper(root.left);
+        int rightDepth = isBalancedHelper(root.right);
+        if (Math.abs(leftDepth - rightDepth) > 1) {
+            isBalanced = false;
+        }
+        // 不太清楚下面这句为什么不能正常运行
+		//isBalanced = Math.abs(leftDepth - rightDepth) <= 1 ? true:false;
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
